a8718c64758368fe50f44a02568f299b0dc500de
George CK <georgeck612@gmail.com>
2024-11-13 08:24:02 -0600

    new data

--- mapptamer2.R
+++ mapptamer2.R
@@ -5,9 +5,9 @@ install_github("https://github.com/Uiowa-Applied-Topology/mappeR/tree/cytoscape"
 library(mappeR)
 
 # read aptamer data as dataframes
-edit_edges = as.data.frame(read.csv("data/edit_dist_edges.csv"))
-tree_edges = as.data.frame(read.csv("data/tree_dist_edges.csv"))
-node_data = as.data.frame(read.csv("data/aptamer-data-raw.csv"))
+edit_edges = as.data.frame(read.csv("data/editdists2.csv"))
+tree_edges = as.data.frame(read.csv("data/treedists2.csv"))
+node_data = as.data.frame(read.csv("data/all_aptamers.csv"))
 
 num_datapoints = nrow(node_data)
 
@@ -35,14 +35,21 @@ colnames(tree_dists) = node_data$name
 
 row.names(node_data) = node_data$name
 
+ecc = eccentricity_filter(tree_dists)
+ecc2 = eccentricity_filter(edit_dists)
+
 # run mapper (clusterball flavor) with varying ball radii
 mapptamer1 = create_clusterball_mapper_object(node_data, tree_dists, edit_dists, 40, "single")
 mapptamer2 = create_clusterball_mapper_object(node_data, tree_dists, edit_dists, 20, "single")
 mapptamer3 = create_clusterball_mapper_object(node_data, tree_dists, edit_dists, 10, "single")
 mapptamer4 = create_clusterball_mapper_object(node_data, tree_dists, edit_dists, 5, "single")
+mapptamer5 = create_1D_mapper_object(node_data, tree_dists, ecc, create_width_balanced_cover(min(ecc), max(ecc), 5, 10), "single")
+mapptamer6 = create_1D_mapper_object(node_data, edit_dists, ecc2, create_width_balanced_cover(min(ecc2), max(ecc2), 5, 10), "single")
 
 # visualize in Cytoscape
 cymapper(mapptamer1)
 cymapper(mapptamer2)
 cymapper(mapptamer3)
 cymapper(mapptamer4)
+cymapper(mapptamer5)
+cymapper(mapptamer6)
